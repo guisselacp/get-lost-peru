c8b2c91937e8ab0cfc643e5aaf1f03cd54bf7fe6
Guissela Chumbe <guisselacp@gmail.com>
2024-12-01 01:22:48 +0000

    Add changes About page

--- about/models.py
+++ about/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
 class About(models.Model):
@@ -6,6 +7,7 @@ class About(models.Model):
     Stores a single about me site.
     """
     title = models.CharField(max_length=200)
+    profile_image = CloudinaryField('image', default='placeholder')
     updated_on = models.DateTimeField(auto_now=True)
     content = models.TextField()
 
