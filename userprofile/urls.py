8fa4a233631bd2ceb24a9015e580d570a5bc524b
Guissela Chumbe <guisselacp@gmail.com>
2024-12-06 09:56:33 +0000

    Add if to change the like icon

--- userprofile/urls.py
+++ userprofile/urls.py
@@ -1,7 +1,10 @@
 from . import views
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('', views.userprofile, name='userprofile'),
+    path('blog/', include('blog.urls')),
+    path('mainpage/',include('mainpage.urls')),
+    path('mainpage/', include('django.contrib.auth.urls')),
 ]
 
--- userprofile/views.py
+++ userprofile/views.py
@@ -2,10 +2,14 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from blog.models import Post
 
+#from blog.views import index
+from django.core.paginator import Paginator
 # Create your views here.
 @login_required
 def userprofile(request):
     posts = Post.objects.filter(author=request.user)
 
-    return render(request, 'userprofile/userprofile.html',)
+    return render(request, 'userprofile/userprofile.html', {
+        'posts': posts,
+    })
    
